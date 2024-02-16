97a090a6a7fd978620526a76088d22d898069909
Sergii Maksymov <ua.maximoff@gmail.com>
2024-02-16 22:04:22 +0100

    update Flutter to 3.13.8 and Dart to 3.2.5

--- pkgs/dart/sources.nix
+++ pkgs/dart/sources.nix
@@ -1,24 +1,24 @@
-let version = "3.2.4"; in
+let version = "3.2.5"; in
 { fetchurl }: {
   versionUsed = version;
   "${version}-x86_64-darwin" = fetchurl {
     url = "https://storage.googleapis.com/dart-archive/channels/stable/release/${version}/sdk/dartsdk-macos-x64-release.zip";
-    sha256 = "107sq5m684mxw5k21zfs3iyihzbqkfmh0vpj17qca19rghnxgn02";
+    sha256 = "0jfkf74sfms5hjsxq5r9bdjxgs08liig4xk9ln74291l3qrr2j7c";
   };
   "${version}-aarch64-darwin" = fetchurl {
     url = "https://storage.googleapis.com/dart-archive/channels/stable/release/${version}/sdk/dartsdk-macos-arm64-release.zip";
-    sha256 = "08jbcdm5li30xdy85whdah186g0yiasgl12h6vi1vgld15ifjsab";
+    sha256 = "0lkbxb5jd19a1ia383w05f3rsgzfqka0xkl6b4f2cgj0h1hgj652";
   };
   "${version}-aarch64-linux" = fetchurl {
     url = "https://storage.googleapis.com/dart-archive/channels/stable/release/${version}/sdk/dartsdk-linux-arm64-release.zip";
-    sha256 = "0f40riqcdnjwjnv6si5186h6akrnhnwqrfrgfvm4y0gpblw88c2s";
+    sha256 = "14zkmvciwdpbyz1y587nrx2a2m5jb61vwj531k52dz4hbrhhm15a";
   };
   "${version}-x86_64-linux" = fetchurl {
     url = "https://storage.googleapis.com/dart-archive/channels/stable/release/${version}/sdk/dartsdk-linux-x64-release.zip";
-    sha256 = "1bkrfg3xzkc4zrbl5ialg5jwpb7l0xmrd9aj7x5kwz2v8n8w013n";
+    sha256 = "1c8a84kdyg00211lkm5bnqjjjwc1pv7gj9zw16wiwlazrgv6cpp3";
   };
   "${version}-i686-linux" = fetchurl {
     url = "https://storage.googleapis.com/dart-archive/channels/stable/release/${version}/sdk/dartsdk-linux-ia32-release.zip";
-    sha256 = "0jddia0s7byl7p6qbljp444qs11r8ff58s5fbchcrsmkry3pg8gi";
+    sha256 = "0q1z4bvmwijcgs4ls5i9a6mfnl2xh2scpv7wr54z877h01mypps2";
   };
 }
